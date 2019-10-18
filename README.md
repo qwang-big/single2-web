7e7354a21dd3c3cf737a3c981ce00a727b9e3c00
qwang <qwang-big>
2019-10-18 12:11:37 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# single2-web
+
+[https://qwang-big.github.io/single2-web/](https://qwang-big.github.io/single2-web/)
---
+++ src/plot.r
@@ -0,0 +1,38 @@
+bnorm <- function(d){
+x=split(d, substr(d[,1],1,1))
+m=t(do.call(cbind,lapply(x,function(v) v[order(v[,1]),2])))
+colnames(m)=nuc
+melt(m/rowSums(m))
+}
+
+calcPWM <- function(d){
+x=split(d, nchar(d[,1]))
+lapply(x,function(y){
+t(do.call(rbind,lapply(1:nchar(y[1,1]),function(i){
+m=data.frame(substr(y[,1],i,i),y[,2])
+sapply(nuc,function(d) sum(m[m[,1]==d,2])/sum(m[,2]))
+})))})
+}
+
+logV2 <- function(d){
+d[,2]=log(d[,2]+1)
+d
+}
+
+readModel <- function(f){
+x=readLines(f)
+i=grep('#',x)
+df=data.frame(from=i+1, to=c(i[2:length(i)]-1,length(x)))
+y=apply(df,1,function(d) x[seq(d[1],d[2])])
+names(y)=substr(x[i],2,nchar(x[i]))
+lapply(y, function(d){
+df2 <- do.call(rbind,strsplit(d,'\t'))
+setNames(
+    as.data.frame(lapply(1:ncol(df2), function(i) {
+      type.convert(df2[,i], as.is = TRUE)}), stringsAsFactors = FALSE), 
+  paste0('V', 1:ncol(df2)))
+})
+}
+
+nuc=c('A','C','G','T')
+
